07c4c960f735952720e2392d664bf50b89629448
Brandt Keller <brandt.keller@defenseunicorns.com>
2022-12-18 10:56:09 -0800

    Updating local dev tf and big bang config

--- 1-Infrastructure/modules/dev-node/main.tf
+++ 1-Infrastructure/modules/dev-node/main.tf
@@ -34,6 +34,30 @@ resource "proxmox_vm_qemu" "dev_node" {
     ]
   }
 
+  provisioner "file" {
+    source      = "setup.sh"
+    destination = "/tmp/setup.sh"
+    connection {
+      type     = "ssh"
+      user     = "dev"
+      private_key = file("${var.ssh_priv_key_path}")
+      host     = var.ip_addr
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/setup.sh",
+      "/tmp/setup.sh",
+    ]
+    connection {
+      type     = "ssh"
+      user     = "dev"
+      private_key = file("${var.ssh_priv_key_path}")
+      host     = var.ip_addr
+    }
+  }
+
   # Cloud Init Settings
   ipconfig0  = "ip=${var.ip_addr}/24,gw=192.168.1.1"
   nameserver = var.nameservers
