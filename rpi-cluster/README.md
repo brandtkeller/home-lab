e5cbbb51823cd900415fb7033f51c73cd7d7dfa0
Brandt Keller <brandt.keller@defenseunicorns.com>
2024-05-26 18:50:41 +0000

    feat(rpi): install k3s across infrastructure

--- rpi-cluster/README.md
+++ rpi-cluster/README.md
@@ -7,3 +7,7 @@
 ## Intent
 Provide a lightweight (power consumption) kubernetes cluster for orchestration of services with lower resource demands.
 
+## Requirements
+- Add `cgroup_enable=memory cgroup_memory=1` to the end of `/boot/cmdline.txt`
+- Unique Hostnames
+- Create a unique token for cluster (replacing current stub)
--- rpi-cluster/infrastructure/artifacts/k3s-install.sh
+++ rpi-cluster/infrastructure/artifacts/k3s-install.sh
@@ -1,11 +1,9 @@
-# RPI K3S Cluster
+#!/bin/bash
 
-```
 cp k3s-arm64 /usr/local/bin/k3s
 chmod +x /usr/local/bin/k3s
 mkdir -p /var/lib/rancher/k3s/agent/images/
 mkdir -p /etc/rancher/k3s/
 cp k3s-airgap-images-arm64.tar.zst /var/lib/rancher/k3s/agent/images/
-cp primary-config.yaml /etc/rancher/k3s/config.yaml
+cp config.yaml /etc/rancher/k3s/config.yaml
 INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC='server' sh /root/install.sh
-```
